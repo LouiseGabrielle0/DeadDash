382ea6d47b08c0987b809230e016e45430968e48
Louise <louisegabriellecc@gmail.com>
2022-04-14 21:52:14 +0100

    Working on issue with buttons

--- js/startgamebutton.js
+++ js/startgamebutton.js
@@ -1,13 +1,4 @@
 
 document.getElementById("ready-player-one-start").onclick = function (){
-    location.href = "../startgame.html"
+    location.href = "../startgame.html";
 }
-
-document.getElementById("ready-player-one-nextlevel").onclick = function (){
-    location.href = "../startgame.html"
-}
-
-document.getElementById("ready-player-one-startagain").onclick = function (){
-    location.href = "../startgame.html"
-}
-
